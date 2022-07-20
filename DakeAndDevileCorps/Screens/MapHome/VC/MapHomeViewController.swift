6d6b84362c630a3ddb686225476cf25d72220b7c
SujinKim <mrmagnet777@gmail.com>
2022-07-21 07:26:50 +0900

    [ADD] 메인맵에 서치바 삽입(#18)

--- DakeAndDevileCorps/Screens/MapHome/VC/MapHomeViewController.swift
+++ DakeAndDevileCorps/Screens/MapHome/VC/MapHomeViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class MapHomeViewController: UIViewController {
 
+    let searchBarView: SearchBarView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(SearchBarView())
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureLayout()
     }
 
+    func configureLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        view.addSubview(searchBarView)
+        NSLayoutConstraint.activate([
+            searchBarView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 8),
+            searchBarView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            searchBarView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            
+        ])
+    }
 
 }
 
