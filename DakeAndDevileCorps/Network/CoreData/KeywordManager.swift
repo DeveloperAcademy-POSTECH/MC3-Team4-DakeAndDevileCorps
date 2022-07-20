a317ee3628ebec78f016358b692052aac0781a32
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-07-20 15:48:21 +0900

    [Add] CoreData Keyword Manager 생성 및 폴더 변경(#13)

---
+++ DakeAndDevileCorps/Network/CoreData/KeywordManager.swift
@@ -0,0 +1,83 @@
+//
+//  KeywordManager.swift
+//  DakeAndDevileCorps
+//
+//  Created by SHIN YOON AH on 2022/07/20.
+//
+
+import CoreData
+import UIKit
+
+final class KeywordManager {
+    
+    // MARK: - properties
+    
+    static var shared: KeywordManager = KeywordManager()
+    
+    private let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    private lazy var context = appDelegate?.persistentContainer.viewContext
+    
+    // MARK: - init
+    
+    private init() { }
+    
+    // MARK: - func
+    
+    func loadFromCoreData<T: NSManagedObject>(request: NSFetchRequest<T>) -> [T] {
+        guard let context = self.context else { return [] }
+        do {
+            let results = try context.fetch(request)
+            return results
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+    
+    func saveRecentSearch(term: String) {
+        guard let context = self.context,
+              let entity = NSEntityDescription.entity(forEntityName: "Keywords", in: context),
+              let recentTerms = NSManagedObject(entity: entity, insertInto: context) as? Keywords
+        else { return }
+        
+        recentTerms.term = term
+
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @discardableResult
+    func delete<T: NSManagedObject>(at term: String, request: NSFetchRequest<T>) -> Bool {
+        request.predicate = NSPredicate(format: "keyword = %@", NSString(string: term))
+
+        do {
+            if let recentTerms = try context?.fetch(request) {
+                if recentTerms.count == 0 { return false }
+                context?.delete(recentTerms[0])
+                try context?.save()
+                return true
+            }
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+
+        return false
+    }
+    
+    @discardableResult
+    func deleteAll<T: NSManagedObject>(request: NSFetchRequest<T>) -> Bool {
+        let request: NSFetchRequest<NSFetchRequestResult> = T.fetchRequest()
+        let delete = NSBatchDeleteRequest(fetchRequest: request)
+        do {
+            try self.context?.execute(delete)
+            return true
+        } catch {
+            return false
+        }
+    }
+}
+
