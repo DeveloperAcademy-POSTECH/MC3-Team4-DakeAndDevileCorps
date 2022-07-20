38df9cc599f503562e20f02de730a534f635212e
SujinKim <mrmagnet777@gmail.com>
2022-07-20 23:03:00 +0900

    [FEAT] 지도 뷰 삽입 및 레이아웃 지정(#14)

--- DakeAndDevileCorps/Screens/MapHome/VC/MainMapViewController.swift
+++ DakeAndDevileCorps/Screens/MapHome/VC/MainMapViewController.swift
@@ -5,10 +5,13 @@
 //  Created by Seungyun Kim on 2022/07/18.
 //
 
+import MapKit
 import UIKit
 
 class MainMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
