cdd2827abbff230f97977bbad4d3c6f6212058ac
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-07-27 16:47:22 +0900

    [Add] Write Review VC 생성(#48)

---
+++ DakeAndDevileCorps/Screens/Store/VC/WriteReviewViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WriteReviewViewController.swift
+//  DakeAndDevileCorps
+//
+//  Created by SHIN YOON AH on 2022/07/27.
+//
+
+import UIKit
+
+final class WriteReviewViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
