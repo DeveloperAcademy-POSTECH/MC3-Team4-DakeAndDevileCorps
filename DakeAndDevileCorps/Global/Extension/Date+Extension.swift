5588927282ea64ccd66d88c7b5a2cdacbeb743dd
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-08-01 23:27:48 +0900

    [Feat] locale 추가 완료(#113)

--- DakeAndDevileCorps/Global/Extension/Date+Extension.swift
+++ DakeAndDevileCorps/Global/Extension/Date+Extension.swift
@@ -11,6 +11,7 @@ extension Date {
     static func getCurrentDate(with format: String) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = format
+        formatter.locale = Locale(identifier: "ko")
         let currentTimeString = formatter.string(from: Date())
         return currentTimeString
     }
