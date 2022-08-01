cc398edfc028acaf3cc17adcc4dce2e32498528d
ccdkk <cdk243@gmail.com>
2022-08-01 21:56:51 +0900

    [Feat] 세로모드 고정 (#108)

--- DakeAndDevileCorps/Global/Supports/AppDelegate.swift
+++ DakeAndDevileCorps/Global/Supports/AppDelegate.swift
@@ -58,5 +58,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
 }
 
